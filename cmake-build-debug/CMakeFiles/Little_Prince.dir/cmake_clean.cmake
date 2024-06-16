8c4a986ae6bebcfbed191ec655cbaca99015e651
Marlinpa <mariannaxlinp@gmail.com>
2024-06-17 02:25:03 +0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Little_Prince.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Little_Prince.dir/main.cpp.obj"
+  "Little_Prince.exe"
+  "Little_Prince.exe.manifest"
+  "Little_Prince.pdb"
+  "libLittle_Prince.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Little_Prince.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
